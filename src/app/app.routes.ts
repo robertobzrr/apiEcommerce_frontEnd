ff583021c9c2ff00c62c2921fd64ed4ada586716
robertobzrr <robertobezerrafh@outlook.com>
2025-12-01 12:25:48 -0300

    add: all base components needed and configure the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';      
+import { RegisterComponent } from './components/register/register.component';
+import { CartComponent } from './components/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'products', component: ProductsComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'cart', component: CartComponent }
+
+];
