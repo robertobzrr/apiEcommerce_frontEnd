377e858d4cd1a0773a8d8a9f4e6c68a3fb41a292
robertobzrr <robertobezerrafh@outlook.com>
2025-12-01 19:03:11 -0300

    fix: re-doing a lot after break changing foulder local

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { FormsComponent } from '../forms/forms.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsComponent],
+  imports: [],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
