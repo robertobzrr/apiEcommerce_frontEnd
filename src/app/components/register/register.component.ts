22d97ec35b81fcbb28e70e3dc278ddbe25dc1d43
robertobzrr <robertobezerrafh@outlook.com>
2025-12-01 13:07:49 -0300

    add and tested: forms into register page and working

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsComponent } from '../forms/forms.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
