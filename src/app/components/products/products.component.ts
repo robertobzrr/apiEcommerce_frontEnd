fcdea37e760fe72b08632b896740aa9a93cb3792
robertobzrr <robertobezerrafh@outlook.com>
2025-12-02 10:38:56 -0300

    add: layout products page + route/botton for create product

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
