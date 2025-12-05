4792d026edd26533129e4cb95b5c39806388f689
robertobzrr <robertobezerrafh@outlook.com>
2025-12-05 11:47:49 -0300

    add/change: cart html and css to use products from DB

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,41 +1,49 @@
-import { Component, inject } from '@angular/core';
-import { CartService } from '../../services/cart/cart.service';
+import { Component, inject, OnInit } from '@angular/core';
+import { CartService, CartItem } from '../../services/cart/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
 
+  private cartService = inject(CartService);
   
-  
-  name: string = '';
-  description: string = '';
-  price: number = 0;
-
-
-  cartService = inject(CartService);
-
-  constructor() { 
-
-
-    console.log('REGISTER INIATIALIZED');
-  }
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
 
   ngOnInit(){
-    console.log('REGISTER ngOnInit CALLED');
-    console.log('Cart Items:', this.cartService.getItems());
-  }  
-
-  ngOnChanges(){
-    console.log('REGISTER ngOnChanges CALLED');
+    this.loadCartItems();
   }
 
-  ngAfterViewInit(){
-    console.log('REGISTER ngAfterViewInit CALLED');
+  loadCartItems() {
+    this.cartItems = this.cartService.getItems();
+    this.totalPrice = this.cartService.getTotalPrice();
+  }
+
+  removeFromCart(productId: number) {
+    this.cartService.removeItem(productId);
+    this.loadCartItems();
+  }
+
+  updateQuantity(productId: number, quantity: number) {
+    this.cartService.updateQuantity(productId, quantity);
+    this.loadCartItems();
+  }
+
+  checkout() {
+    if (this.cartItems.length === 0) {
+      alert('cart empty');
+      return;
+    }
+    
+    alert(`Cart finished! Total: R$ ${this.totalPrice.toFixed(2)}`);
+    this.cartService.clearCart();
+    this.loadCartItems();
   }
 
 }
